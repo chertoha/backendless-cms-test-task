ad252c7439b8cc4dadf16d395eea746b36f9edba
Anton <a.chertok@akatech.com.ua>
2023-10-28 18:58:24 +0100

    global styles

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-// import { GlobalStyle } from "./styles/global.styled";
+import { GlobalStyle } from "./styles/global.styled";
 import { store } from "redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter basename="/backendless-cms-test-task">
       <Provider store={store}>
-        {/* <GlobalStyle /> */}
+        <GlobalStyle />
         <App />
       </Provider>
     </BrowserRouter>
--- src/styles/global.styled.js
+++ src/styles/global.styled.js
@@ -1,134 +1,66 @@
-// import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
-const { css } = require("@emotion/react");
+export const GlobalStyle = createGlobalStyle`
 
-// export const GlobalStyle = createGlobalStyle`
+*, *::before, *::after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
 
-// *, *::before, *::after {
-//   box-sizing: border-box;
-//   padding: 0;
-//   margin: 0;
-// }
+html,
+body {
+  max-width: 100vw;
+  height: 100vh;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+}
 
-// html,
-// body {
-//   max-width: 100vw;
-//   height: 100vh;
-//   overflow-x: hidden;
-//   scroll-behavior: smooth;
-// }
+body {
+  margin: 0;
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
-// body {
-//   margin: 0;
-//   font-family: 'Montserrat', sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
+  font-size: 16px;
+  font-weight: 400;
+  color: #000000;
 
-//   font-size: 16px;
-//   font-weight: 400;
-//   color: #000000;
+  background-color: #ffffff;
+}
 
-//   background-color: #ffffff;
-// }
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
-// ul {
-//   list-style: none;
-//   margin: 0;
-//   padding: 0;
-// }
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+li {
+  margin: 0;
+  padding: 0;
+}
 
-// h1,
-// h2,
-// h3,
-// h4,
-// h5,
-// h6,
-// p,
-// li {
-//   margin: 0;
-//   padding: 0;
-// }
+input {
+  margin: 0;
+  padding: 0;
+}
 
-// input {
-//   margin: 0;
-//   padding: 0;
-// }
+a {
+  text-decoration: none;
+  transition: color 250ms ease-in;
+}
 
-// a {
-//   text-decoration: none;
-//   transition: color 250ms ease-in;
-// }
-
-// button {
-//   cursor: pointer;
-//   background-color: transparent;
-//   border: none;
-//   transition: background-color 250ms ease-in;
-// }
-// `;
-
-export const globalStyles = css`
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-    padding: 0;
-    margin: 0;
-  }
-
-  html,
-  body {
-    max-width: 100vw;
-    height: 100vh;
-    overflow-x: hidden;
-    scroll-behavior: smooth;
-  }
-
-  body {
-    margin: 0;
-    font-family: "Montserrat", sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-
-    font-size: 16px;
-    font-weight: 400;
-    color: #000000;
-
-    background-color: #ffffff;
-  }
-
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  p,
-  li {
-    margin: 0;
-    padding: 0;
-  }
-
-  input {
-    margin: 0;
-    padding: 0;
-  }
-
-  a {
-    text-decoration: none;
-    transition: color 250ms ease-in;
-  }
-
-  button {
-    cursor: pointer;
-    background-color: transparent;
-    border: none;
-    transition: background-color 250ms ease-in;
-  }
+button {
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  transition: background-color 250ms ease-in;
+}
 `;
